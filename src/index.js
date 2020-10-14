4f319778e8e9a3357795c103a729efa15fbf9448
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-10-13 21:15:29 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {BrowserRouter as Router} from "react-router-dom"
+ReactDOM.render(
+  <Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
+);
